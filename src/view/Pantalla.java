0808eba1fffd29c6a540b1c12619f35eca58937e
pablin2402 <pabloperedo04@gmail.com>
2019-10-11 13:41:36 -0400

    syso

--- src/view/Pantalla.java
+++ src/view/Pantalla.java
@@ -3,9 +3,10 @@ package view;
 import model.Persona;
 
 public class Pantalla {
-	
+
 	public static void main(String[] args) {
-		Persona persona = new Persona(1,"John", "Doe");
+		Persona persona = new Persona(1, "John", "Doe");
 		System.out.println(persona);
+		System.out.println("s");
 	}
 }
