44d6a3204da65df7d66801bc210eac02295a4a87
Marcelo López <marcelo.lopez@live.com>
2019-10-11 12:45:25 -0400

    Segunda subida

---
+++ src/view/Pantalla.java
@@ -0,0 +1,11 @@
+package view;
+
+import model.Persona;
+
+public class Pantalla {
+	
+	public static void main(String[] args) {
+		Persona persona = new Persona(1,"John", "Doe");
+		System.out.println(persona);
+	}
+}
