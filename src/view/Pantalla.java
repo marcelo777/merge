231646debadd3cdd7293196c482d1cdc8c18fed3
KOrtizLedezma <kenethortiz2016@gmail.com>
2019-10-11 13:15:37 -0400

    Atributos Creados

--- src/view/Pantalla.java
+++ src/view/Pantalla.java
@@ -5,7 +5,7 @@ import model.Persona;
 public class Pantalla {
 	
 	public static void main(String[] args) {
-		Persona persona = new Persona(1,"John", "Doe");
+		Persona persona = new Persona(1,"John", "Doe", "Hombre");
 		System.out.println(persona);
 	}
 }
